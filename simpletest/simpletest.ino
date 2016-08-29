b9a9d63dcae0ca9f5f02768b3bdc6f40ced37b46
bbbnh <nhedler@bbbind.com>
2016-08-29 17:27:26 -0400

    updated simple test with working paramters

--- simpletest/simpletest.ino
+++ simpletest/simpletest.ino
@@ -1,9 +1,13 @@
-int duty = 0x80;
+int duty = 0x40;
 
 void setup() {
-  pinMode(10, OUTPUT);
+  pinMode(10, OPEN);
+  pinMode(9, OPEN);
+  analogWriteFrequency(400);
+  analogWriteResolution(8);
 }
 
 void loop() {
   analogWrite(10, duty);
+  analogWrite(9, 0xff - duty);
 }
