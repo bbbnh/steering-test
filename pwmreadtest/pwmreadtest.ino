24a7d87574bf62b198eba8157d614c201f11bcb4
bbbnh <nhedler@bbbind.com>
2016-08-31 16:07:02 -0400

    added comments, forgot to save before last commit

--- pwmreadtest/pwmreadtest.ino
+++ pwmreadtest/pwmreadtest.ino
@@ -15,19 +15,22 @@ void setup() {
 }
 
 void loop() {
+  // this is a bit of a brute-force method, but it works
   int totalPulse = 0;
   int pulseCount = 0;
   int currentTime = 0;
   int startTime = millis();
   while (currentTime - startTime < 100) {
-    totalPulse += pulseIn(inPin, HIGH);
-    pulseCount += 1;
+    totalPulse += pulseIn(inPin, HIGH); // track total pulse-high time
+    pulseCount += 1; // track number of pulses
     currentTime = millis();
   }
   int timeDiff = currentTime - startTime;
   float freq = pulseCount / 100.0; // kHz
   int pulse = totalPulse / pulseCount; // microseconds
   float duty = pulse * freq / timeDiff * 10; // percent
+
+  // formatted output
   Serial.print("Frequency\tPulse Width\tDuty\n");
   Serial.print(freq, 3);
   Serial.print(" kHz\t");
