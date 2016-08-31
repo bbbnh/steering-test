f683cda25c3766792bc34ec24df5ac02bdbe6299
bbbnh <nhedler@bbbind.com>
2016-08-31 16:01:39 -0400

    added PWM analysis sketch

---
+++ pwmreadtest/pwmreadtest.ino
@@ -0,0 +1,38 @@
+/*
+ * This test checks the frequency and duty cycle of a PWM signal.
+ * 
+ * Serial output includes frequency, pulse width, and duty percentage. Input
+ * is on pin 14 (A0). Open-drain input is expected; thus, input pullups are
+ * enabled. 
+ */
+
+const byte inPin = 14;
+
+void setup() {
+  pinMode(inPin, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int totalPulse = 0;
+  int pulseCount = 0;
+  int currentTime = 0;
+  int startTime = millis();
+  while (currentTime - startTime < 100) {
+    totalPulse += pulseIn(inPin, HIGH);
+    pulseCount += 1;
+    currentTime = millis();
+  }
+  int timeDiff = currentTime - startTime;
+  float freq = pulseCount / 100.0; // kHz
+  int pulse = totalPulse / pulseCount; // microseconds
+  float duty = pulse * freq / timeDiff * 10; // percent
+  Serial.print("Frequency\tPulse Width\tDuty\n");
+  Serial.print(freq, 3);
+  Serial.print(" kHz\t");
+  Serial.print(pulse);
+  Serial.print(" us\t\t");
+  Serial.print(duty);
+  Serial.print("%\n\n");
+}
